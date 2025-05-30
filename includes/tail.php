edc5c77a9a2e3a66de41c99709b2d46111c7ed01
Alif <ahmedalif1010@gmail.com>
2025-05-30 23:23:21 +1000

    final touch

--- includes/tail.php
+++ includes/tail.php
@@ -2,6 +2,7 @@
     <p>Code by Ahmed Alif Swopno</p>
     <p>Student ID: 8068380</p>
     <p><a href="mailto:aas208@uowmail.edu.au">📧 aas208@uowmail.edu.au</a></p>
+    <p><a href="https://github.com/thealiflab/ev-charging-portal">📌 Github link</a></p>
     <p><a href="http://thealiflab.github.io">🌐 thealiflab.github.io</a></p>
   </footer>
 
